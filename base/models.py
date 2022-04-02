b47c3adef7cbf90d19c25b53de7a0af478116cfc
ayda76 <aydavahedfrough@yahoo.com>
2022-04-02 08:31:26 +0430

    created profile and rooms

--- base/models.py
+++ base/models.py
@@ -13,6 +13,8 @@ class Room(models.Model):
     topic=models.ForeignKey(Topic, on_delete=models.SET_NULL,null=True)
     name=models.CharField(max_length=300)
     description=models.TextField(null=True,blank=True)
+    participants=models.ManyToManyField(User,related_name='paricipants',blank=True)
+    
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True)
 
@@ -30,5 +32,8 @@ class Message(models.Model):
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering=['-updated','-created']
+
     def __str__(self):
-        return self.body
+        return self.body[0:50]
--- base/urls.py
+++ base/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('register/',views.registerPage, name='register'),
     path('',views.home, name='home'),
     path('room/<str:pk>/',views.room, name='room'),
+    path('profile/<str:pk>/',views.userProfile, name='userProfile'),
     path('createRoom/',views.createRoom, name='createRoom'),
     path('updateRoom/<str:pk>/',views.updateRoom, name='updateRoom'),
     path('deleteRoom/<str:pk>/',views.deleteRoom, name='deleteRoom'),
+    path('deleteMessage/<str:pk>/',views.deleteMessage, name='deleteMessage'),
 ]
